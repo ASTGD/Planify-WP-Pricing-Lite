aabd36a2217a7de56ec15c1597da88e2e0d352ca
Sharwat Shafin <sharwatislam@gmail.com>
2025-09-30 17:04:36 +0600

    fix(admin): use explicit CPT labels to avoid 'Add Post' submenu; show 'Add New' for Tables/Plans

--- includes/class-pwpl-cpt.php
+++ includes/class-pwpl-cpt.php
@@ -7,9 +7,27 @@ class PWPL_CPT {
         add_action( 'init', [ $this, 'register_plans_cpt' ] );
     }
 
+    private function labels( $singular, $plural ) {
+        return [
+            'name'               => $plural,
+            'singular_name'      => $singular,
+            'menu_name'          => $plural,
+            'name_admin_bar'     => $singular,
+            'add_new'            => __( 'Add New', 'planify-wp-pricing-lite' ),
+            'add_new_item'       => sprintf( __( 'Add New %s', 'planify-wp-pricing-lite' ), $singular ),
+            'new_item'           => sprintf( __( 'New %s', 'planify-wp-pricing-lite' ), $singular ),
+            'edit_item'          => sprintf( __( 'Edit %s', 'planify-wp-pricing-lite' ), $singular ),
+            'view_item'          => sprintf( __( 'View %s', 'planify-wp-pricing-lite' ), $singular ),
+            'all_items'          => sprintf( __( 'All %s', 'planify-wp-pricing-lite' ), $plural ),
+            'search_items'       => sprintf( __( 'Search %s', 'planify-wp-pricing-lite' ), $plural ),
+            'not_found'          => __( 'No items found.', 'planify-wp-pricing-lite' ),
+            'not_found_in_trash' => __( 'No items found in Trash.', 'planify-wp-pricing-lite' ),
+        ];
+    }
+
     public function register_tables_cpt() {
         register_post_type( 'pwpl_table', [
-            'label' => __( 'Pricing Tables', 'planify-wp-pricing-lite' ),
+            'labels' => $this->labels( __( 'Pricing Table', 'planify-wp-pricing-lite' ), __( 'Pricing Tables', 'planify-wp-pricing-lite' ) ),
             'public' => false,
             'show_ui' => true,
             'menu_position' => 25,
@@ -22,7 +40,7 @@ class PWPL_CPT {
 
     public function register_plans_cpt() {
         register_post_type( 'pwpl_plan', [
-            'label' => __( 'Plans', 'planify-wp-pricing-lite' ),
+            'labels' => $this->labels( __( 'Plan', 'planify-wp-pricing-lite' ), __( 'Plans', 'planify-wp-pricing-lite' ) ),
             'public' => false,
             'show_ui' => true,
             'show_in_menu' => 'edit.php?post_type=pwpl_table',
@@ -31,4 +49,3 @@ class PWPL_CPT {
         ] );
     }
 }
-
