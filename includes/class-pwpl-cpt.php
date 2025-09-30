23bb9e4fe1e8eb9b22d87ea2826a83ae75b408dd
Sharwat Shafin <sharwatislam@gmail.com>
2025-09-30 17:16:33 +0600

    fix(admin): explicitly add 'Add New Plan' submenu under Pricing Tables

--- includes/class-pwpl-cpt.php
+++ includes/class-pwpl-cpt.php
@@ -5,6 +5,8 @@ class PWPL_CPT {
     public function init() {
         add_action( 'init', [ $this, 'register_tables_cpt' ] );
         add_action( 'init', [ $this, 'register_plans_cpt' ] );
+        // Ensure desired submenu layout under Pricing Tables
+        add_action( 'admin_menu', [ $this, 'ensure_submenus' ], 20 );
     }
 
     private function labels( $singular, $plural, $add_new_text = null ) {
@@ -50,5 +52,19 @@ class PWPL_CPT {
             'hierarchical' => false,
             'has_archive' => false,
         ] );
+    }
+
+    public function ensure_submenus() {
+        $parent = 'edit.php?post_type=pwpl_table';
+        // Add explicit "Add New Plan" entry if WP doesn't add it automatically
+        add_submenu_page(
+            $parent,
+            __( 'Add New Plan', 'planify-wp-pricing-lite' ),
+            __( 'Add New Plan', 'planify-wp-pricing-lite' ),
+            'edit_posts',
+            'post-new.php?post_type=pwpl_plan',
+            '__return_null',
+            15
+        );
     }
 }
